c6cc64f74a4b4bd77ed7bb818232e7080f845382
Lukas Matousek <matousek1199@gmail.com>
2025-03-10 21:37:00 +0100

    feat: more statistics

--- common/services/statistics.py
+++ common/services/statistics.py
@@ -3,6 +3,8 @@ from common.models import Flight
 
 def get_flights_statistics(flights: dict[str, Flight]) -> object:
     number_of_flights = len(flights)
+    if number_of_flights == 0:
+        return {}
 
     average_detection_time_diff_compared_to_aftn = None
     number_of_parsing_failures = 0
@@ -19,6 +21,9 @@ def get_flights_statistics(flights: dict[str, Flight]) -> object:
         if flight.detection_failure:
             number_of_parsing_failures += 1
 
+    if average_detection_time_diff_compared_to_aftn is not None:
+        average_detection_time_diff_compared_to_aftn =average_detection_time_diff_compared_to_aftn / number_of_flights
+
     b2b_messages_number = 0
     aftn_messages_number = 0
     for key, flight in flights.items():
@@ -33,10 +38,33 @@ def get_flights_statistics(flights: dict[str, Flight]) -> object:
         'number_of_parsing_failures': number_of_parsing_failures,
         'number_of_flights_with_identical_slots': number_of_flights_with_identical_slots,
         'number_of_flights_with_identical_slots_relative': number_of_flights_with_identical_slots / number_of_flights * 100,
-        'average_detection_time_diff': average_detection_time_diff_compared_to_aftn / number_of_flights,
+        'average_detection_time_diff': average_detection_time_diff_compared_to_aftn,
+        'duplicate_slot_detection_number': get_number_duplicate_slot_detection(flights),
+        'not_detected_slots_number': get_number_not_detected_slots(flights),
         'average_num_of_messages': {
             'b2b_messages_number': b2b_messages_number,
             'aftn_messages_number': aftn_messages_number,
         },
     }
 
+def get_number_duplicate_slot_detection(flights: dict[str, Flight]) -> int:
+    number = 0
+    for key, flight in flights.items():
+        aftn = flight.get_aftn_messages()
+        aftn = [item for item in aftn if item.message_type != 'ACK']
+        b2b = flight.get_b2b_messages()
+
+        if len(b2b) > len(aftn):
+            number += 1
+    return number
+
+def get_number_not_detected_slots(flights: dict[str, Flight]) -> int:
+    number = 0
+    for key, flight in flights.items():
+        aftn = flight.get_aftn_messages()
+        aftn = [item for item in aftn if item.message_type != 'ACK']
+        b2b = flight.get_b2b_messages()
+
+        if len(b2b) < len(aftn):
+            number += 1
+    return number
